5bf6d32ad5424fe0e79452026f7a65b79eae0107
Djaber Solimani <dso@affini-tech.com>
2021-01-20 16:13:04 +0100

    colon for tokenization

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import pandas as pd
 def tokenization(tweet):
     tokens=[]
     for word in tweet.split():
+        if "." in word:
+            if word==".":
+                continue
+            else:
+                sub_words=word.split(".")
+                for sub_word in sub_words:
+                    if sub_word!="":
+                        print("sub word of ",word," ",sub_word)
+                        tokens.append(sub_word.lower())
         if word[0]=="@":
             word=word[1:]
-            tokens.append(word)
+            tokens.append(word.lower())
         elif word[0]=="#":
             word=word[1:]
             start_index=0
             for i,letter in enumerate(word):
                 if letter.isupper():
                     if i!=0:
-                        tokens.append(word[start_index:i])
+                        tokens.append(word[start_index:i].lower())
                     start_index=i
                         
                 elif i==len(word)-1:
-                    tokens.append(word[start_index:])
+                    tokens.append(word[start_index:].lower())
                 else:
                     continue
     return tokens
@@ -26,10 +35,10 @@ data=[]
 with open("./train.txt","r") as file:
     for line in file:
         metadata,tweet = line[:14],line[14:]
-        _,type_tweet,company = metadata.split(",")
+        _,tag,company = metadata.split(",")
         company=company[:-1]
         tokens=tokenization(tweet)
-        data.append((type_tweet,company,tokens))
+        data.append((tag,company,tokens))
 
-df=pd.DataFrame(data,columns=["Type","Company","Tweet"])
-
+df=pd.DataFrame(data,columns=["Tag","Company","Tweet"])
+print(df.head(n=100))
